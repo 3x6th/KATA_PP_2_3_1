46f83e95616692cadc7b5ca590e6556d315eb83c
smashbuster <smashbaster@gmail.com>
2022-10-16 23:44:06 +0700

    initial commit

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,70 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surName;
+    @Column(name = "department")
+    private String department;
+
+    public User() {
+    }
+
+    public User(int id, String name, String surName, String department) {
+        this.id = id;
+        this.name = name;
+        this.surName = surName;
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
